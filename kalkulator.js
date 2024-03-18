5624d1f346a8722d9319a10dbf1c924bfe394ea0
nadellasafitri <161214715+nadellasafitri@users.noreply.github.com>
2024-03-18 11:07:00 +0700

    v1.1

--- kalkulator.js
+++ kalkulator.js
@@ -3,3 +3,9 @@ function tambah(a, b) {
     return a + b;
 }
 console.log("Hasil penambahan: " + tambah(5, 3));
+
+// Operasi Pengurangan
+function kurang(a, b) {
+    return a - b;
+}
+console.log("Hasil pengurangan: " + kurang(5, 3));
