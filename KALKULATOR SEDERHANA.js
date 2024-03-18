33c1abb065ad011b4ac05e48a65989093e130cdb
nadellasafitri <161214715+nadellasafitri@users.noreply.github.com>
2024-03-18 11:45:59 +0700

    v1.2

--- KALKULATOR SEDERHANA.js
+++ KALKULATOR SEDERHANA.js
@@ -9,3 +9,9 @@ function kurang(a, b) {
     return a - b;
 }
 console.log("Hasil pengurangan: " + kurang(5, 3));
+
+// Operasi Perkalian
+function kali(a, b) {
+    return a * b;
+}
+console.log("Hasil perkalian: " + kali(5, 3));
